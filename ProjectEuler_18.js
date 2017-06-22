61af35e5cec7bdc221b689133e71d8a937aab853
Maarten Van den Bergh <Maarten.Van.den.Bergh@gfi.be>
2017-06-22 11:56:58 +0200

    18 done

---
+++ ProjectEuler_18.js
@@ -0,0 +1,46 @@
+var matrix = [
+    [75],
+    [95, 64],
+    [17, 47, 82],
+    [18, 35, 87, 10],
+    [20, 04, 82, 47, 65],
+    [19, 01, 23, 75, 03, 34],
+    [88, 02, 77, 73, 07, 63, 67],
+    [99, 65, 04, 28, 06, 16, 70, 92],
+    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+    [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+    [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]
+];
+
+var x = 0;
+var y = 0;
+//For debug
+var showCount = 0;
+//---------
+var getMaxSumForPath = (x, y) => {
+    var locationValue = matrix[y][x]
+    var maxLeft = locationValue;
+    var maxRight = locationValue;
+
+    if(y+1 < matrix.length){
+        maxLeft += getMaxSumForPath(x, y+1);
+        maxRight += getMaxSumForPath(x+1, y+1);
+    }
+    var max = maxLeft < maxRight ? maxRight : maxLeft;
+    //For debug
+    if(showCount > 0){
+        console.log(["x:", x, " y:", y, " max:", max].join(""));
+        showCount--;
+    }
+    //---------
+    return max;
+}
+
+console.log(getMaxSumForPath(0,0));
+
+
+
